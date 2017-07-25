82ef32051ba916db9e1fb5e5cf851d9f1f52babb
mspagnoletti <mspagnoletti@PC0329>
2017-07-25 14:39:02 +0100

    first commit

---
+++ MarcoWebsite/MarcoWebsite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MarcoWebsite.MvcApplication" Language="C#" %>
---
+++ MarcoWebsite/MarcoWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MarcoWebsite.Startup))]
+namespace MarcoWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
